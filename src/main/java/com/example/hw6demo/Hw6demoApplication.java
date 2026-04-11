11a30c62152f12d73f078e4420007ecd4df78e16
John Cordova <cordovamedij1@montclair.edu>
2026-04-11 12:16:04 -0400

    initial Spring Boot project for HW6

---
+++ src/main/java/com/example/hw6demo/Hw6demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw6demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw6demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw6demoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hw6demo/Hw6demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw6demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw6demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
